cd9f4237f1cd36de9b04399c0847a04923ca5712
Cody Frank <cody.frank30@gmail.com>
2019-06-02 18:24:48 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -10,8 +10,9 @@ class Student
 
   def self.create_from_collection(students_array)
     students_array.each do |info|
-      self.new(info)
+      student = self.new(info)
     end
+    student
   end
 
   def add_student_attributes(attributes_hash)
